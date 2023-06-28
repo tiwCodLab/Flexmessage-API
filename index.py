27d749ca5de3617fcb3dd00be10e7610bf664058
samarn13 <samarn.sae@gmail.com>
2023-06-28 16:56:09 +0700

    commit core

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
 from routes.flexaver_routes import user
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 app.include_router(user)
