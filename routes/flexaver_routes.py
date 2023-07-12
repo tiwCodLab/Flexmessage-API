a88baea8e9bfa9c3aa3fdee1b30c52f87f77d5fd
tiw_13 <samarn.sae@gmail.com>
2023-07-12 11:38:37 +0700

    commit main

--- routes/flexaver_routes.py
+++ routes/flexaver_routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter,HTTPException,Body, UploadFile,File,Form
-from models.flexaver_models import Flexmessage ,CodeDict
-from schemas.flexaver_schema import  datas_serializer
+from fastapi import APIRouter, HTTPException, Body, UploadFile, File, Form
+from models.flexaver_models import Flexmessage, CodeDict
+from schemas.flexaver_schema import datas_serializer
 from config.db import collection
 from config.firebase_config import config
 import pyrebase
@@ -8,8 +8,10 @@ import pyrebase
 user = APIRouter()
 
 current_id = 0
+
+
 @user.post("/api/")
-async def create_flexmessage(data:Flexmessage):
+async def create_flexmessage(data: Flexmessage):
     global current_id
     try:
         # Create the data to be inserted
@@ -20,8 +22,10 @@ async def create_flexmessage(data:Flexmessage):
         return {"status": "Success"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
 # upload img
+
+
 @user.post("/upload/image/")
 async def upload_file(file: UploadFile = File(...)):
     try:
@@ -34,19 +38,19 @@ async def upload_file(file: UploadFile = File(...)):
 
         file.filename = f"{new_counter}.jpg"
         contents = await file.read()
-    
+
         storage.child(file.filename).put(contents)
         file_url = storage.child(file.filename).get_url(None)
         print(file_url)
     # Insert data into the database
-        return {"status": "Success","urlimg":file_url}
+        return {"status": "Success", "urlimg": file_url}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
 
 # เพิ่มข้อมูล
 @user.post("/api/flexmessage/")
-async def create_flex_message(data: Flexmessage ):
+async def create_flex_message(data: Flexmessage):
     try:
         # เรียกดูในฐานข้อมูลว่าถึง id ไหน
         latest_document = collection.find().sort('id', -1).limit(1).next()
@@ -75,8 +79,9 @@ async def create_flex_message(data: Flexmessage ):
 
 # เรียกดูข้อมูลทั้งหมด
 @user.get("/api/flexmessage/")
-async def get_all_flex_messages(page: int = 1, per_page: int = 10):
+async def get_all_flex_messages(page: int = 1):
     try:
+        per_page = 10
         # Calculate the starting index
         start_index = (page - 1) * per_page
         # Query the database to get a slice of data
@@ -87,18 +92,24 @@ async def get_all_flex_messages(page: int = 1, per_page: int = 10):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# เรียกดูข้อมูล ตาม ID นั้นๆ
+
+
 @user.get("/api/flexmessage/{message_id}")
 async def get_flex_message(message_id: int):
     try:
         flex_message = collection.find_one({"id": message_id})
         if flex_message:
             serialized_data = datas_serializer([flex_message])
-            return { "message": serialized_data}
+            return {"message": serialized_data}
         else:
-            raise HTTPException(status_code=404, detail="Flex message not found")
-    
+            raise HTTPException(
+                status_code=404, detail="Flex message not found")
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# แก้ไขข้อมูล
 
 
 @user.put("/api/flexmessage/{message_id}")
@@ -117,11 +128,14 @@ async def update_flex_message(message_id: int, name: str = Body(...), category: 
             collection.update_one({"id": message_id}, {"$set": flex_message})
             return {"status": "Success"}
         else:
-            raise HTTPException(status_code=404, detail="Flex message not found")
+            raise HTTPException(
+                status_code=404, detail="Flex message not found")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+# ลบข้อมูล
+
 
 @user.delete("/api/flexmessage/{message_id}")
 async def delete_flex_message(message_id: int):
@@ -130,22 +144,22 @@ async def delete_flex_message(message_id: int):
         if result.deleted_count > 0:
             return {"status": "Ok", "message": "Flex message deleted"}
         else:
-            raise HTTPException(status_code=404, detail="Flex message not found")
-    
+            raise HTTPException(
+                status_code=404, detail="Flex message not found")
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @user.get("/api/flexmessage/category/{category}")
 async def get_flex_messages_by_category(category: str):
     try:
         # Query the database to get flex messages with the specified category
         flex_messages = collection.find({"category": category})
-        
+
         serialized_data = datas_serializer(flex_messages)
-        
+
         return {"message": serialized_data}
-    
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
